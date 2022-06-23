7f555cc18f21a9f3548af7e746dbd6e09f98a8be
shangguanjunwei <shangguanjunwei@5i5j.com>
2022-06-23 16:36:25 +0800

    config

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   // lintOnSave: false,
   pluginOptions: {
     electronBuilder: {
-      mainProcessFile: "src/electron/background.js", // electron 入口文件地址
+      mainProcessFile: "src/electron/background.js", // electron 入口文件地址，默认为 src/background.js
       preload: "src/electron/preload.js", // electron 预加载文件路径 ，这里配置完后 electron 入口文件中也必须配置一次
       builderOptions: {}, // 打包配置
       // outputDir: "sgjw-electron-app", // 打包输出文件夹名称，默认为 dist_electron
